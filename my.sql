10a5a2990a9348ee8c693c0f94fff564e6ad9215
Vu Minh Quan <quanvm@coccoc.com>
2021-09-16 19:07:37 +0700

    TIL 20210916

---
+++ my.sql
@@ -0,0 +1,33 @@
+-- Dump DB ---
+mysqldump --defaults-file=/etc/mysql/debian.cnf --routines --events --triggers ${DB} ${TABLE:}> ${DB}.${TABLE:}.${DATE}.sql
+
+-- DDL ---
+/* with PT-ONLINE-SCHEMA-CHANGE
+https://www.percona.com/doc/percona-toolkit/3.0/pt-online-schema-change.html */
+--- For big tables: --max-load Threads_running=20000 --critical-load Threads_running=20000
+/usr/bin/pt-online-schema-change --defaults-file=/etc/mysql/debian.cnf --alter='${DDL_QUERY1}, ${DDL_QUERY2}' D=${DB},t=${TABLE} --dry-run
+
+--- Insight ---
+/* Size of DB, Table
+https://chartio.com/resources/tutorials/how-to-get-the-size-of-a-table-in-mysql/ */
+SELECT
+    TABLE_SCHEMA AS 'DB Name',
+    ROUND(SUM(DATA_LENGTH + INDEX_LENGTH) / 1024 / 1024, 1) AS 'DB Size in MB'
+FROM
+    information_schema.TABLES
+WHERE
+    TABLE_SCHEMA = '${DB}'
+GROUP BY
+    TABLE_SCHEMA;
+
+SELECT
+    TABLE_NAME AS `Table`,
+    ROUND((DATA_LENGTH + INDEX_LENGTH) / 1024 / 1024) AS `Size (MB)`
+FROM
+    information_schema.TABLES
+WHERE
+        TABLE_SCHEMA = "${DB}"
+    AND
+        TABLE_NAME = "${TABLE}"
+GROUP BY
+    TABLE_NAME;
